9fb9bf3875c69fc10c5f59d4ee3c6af4d488d260
koisland <koshima789@gmail.com>
2023-04-02 01:56:56 -0700

    Allow lambda access to input s3 bucket.

--- modules/snakemake/lambda.tf
+++ modules/snakemake/lambda.tf
@@ -2,14 +2,30 @@ module "lambda_function" {
   source  = "terraform-aws-modules/lambda/aws"
   version = ">= 3.8.2"
 
-  function_name = "${var.name}-lambda"
-  description   = "Trigger for batch."
-  handler       = "trigger.lambda_handler"
-  runtime       = "python3.8"
-  publish       = true
+  function_name      = "${var.name}-lambda"
+  description        = "Trigger for batch."
+  handler            = "trigger.lambda_handler"
+  runtime            = "python3.8"
+  publish            = true
+  attach_policy_json = true
 
   source_path = "${path.module}/lambda_py"
 
+  # Allow accessing any object in input bucket.
+  policy_json = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [{
+      "Action" : [
+        "s3:*GetObject"
+      ],
+      "Resource" : [
+        "arn:aws:s3:::${module.s3_smk_input.s3_bucket_id}/*"
+      ],
+      "Effect" : "Allow"
+    }]
+  })
+
+  # Allow S3 to trigger.
   allowed_triggers = {
     AllowExecutionFromS3Bucket = {
       service    = "s3"
