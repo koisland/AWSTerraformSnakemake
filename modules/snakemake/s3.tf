51dc023f77adb6c9cf048fcb1ea30ee164c8fb27
koisland <koshima789@gmail.com>
2023-04-02 01:29:42 -0700

    Separated input and output buckets. Switched tf boilerplate for modules. Added s3 notification and lambda trigger function.

--- modules/snakemake/s3.tf
+++ modules/snakemake/s3.tf
@@ -1,31 +1,70 @@
 # tfsec:ignore:aws-s3-enable-bucket-logging
-resource "aws_s3_bucket" "output" {
-  bucket = "${var.name}-output"
-}
+module "s3_smk_output" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = ">= 3.8.2"
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "output_encryption" {
-  bucket = aws_s3_bucket.output.id
+  bucket                  = "${var.name}_output"
+  acl                     = "private"
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  block_public_acls       = true
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = "arn"
-      sse_algorithm     = "aws:kms"
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = "arn"
+        sse_algorithm     = "aws:kms"
+      }
     }
   }
-}
 
-resource "aws_s3_bucket_versioning" "output_versioning" {
-  bucket = aws_s3_bucket.output.id
-  versioning_configuration {
-    status = "Enabled"
+  versioning = {
+    enabled = true
   }
 }
 
-# tflint-ignore: terraform_required_providers
-resource "aws_s3_bucket_public_access_block" "output_access" {
-  bucket                  = aws_s3_bucket.output.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
+# tfsec:ignore:aws-s3-enable-bucket-logging
+module "s3_smk_input" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = ">= 3.8.2"
+
+  bucket                  = "${var.name}_input"
+  acl                     = "private"
   restrict_public_buckets = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  block_public_acls       = true
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = "arn"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "s3_notification" {
+  source  = "terraform-aws-modules/s3-bucket/aws//modules/notification"
+  version = ">= 3.8.2"
+
+  bucket = module.s3_smk_input.s3_bucket_id
+
+  eventbridge = true
+
+  lambda_notifications = {
+    lambda1 = {
+      function_arn  = module.lambda_function.lambda_function_arn
+      function_name = module.lambda_function.lambda_function_name
+      events        = ["s3:ObjectCreated:*"]
+      filter_prefix = "data/"
+      filter_suffix = ".json"
+    }
+  }
 }
